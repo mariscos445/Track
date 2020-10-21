ec181673940cc2f0b3faa6acc7398291e6cfb25e
mariscos <mariscos445@gmail.com>
2020-10-21 03:21:57 -0600

    final

---
+++ arriba.js
@@ -0,0 +1,25 @@
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+    var currentScroll = document.documentElement.scrollTop;
+
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll / 5));
+    }
+}
+
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 300){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 300){
+        buttonUp.style.transform = "scale(0)";
+    }
+
+}
